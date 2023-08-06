e599d844945d334cedaffeeb7bea1c0e6d39682e
Jieff Cavalcanti <jieff.cavalcanti@hotmail.com>
2023-08-05 23:20:26 -0300

    entities content and tests

--- src/infra/app.controller.ts
+++ src/infra/app.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { randomUUID } from 'node:crypto';
+//import { Notification } from 'src/application/entities/notification';
 import { CreateNotificationBody } from './create-notification-body';
+//import { Content } from 'src/application/entities/content';
 
 @Controller('notifications')
 export class AppController {
@@ -9,21 +11,21 @@ export class AppController {
 
   @Get()
   list() {
-    return this.prisma.notification.findMany()
+    return this.prisma.notification.findMany();
   }
 
   @Post()
   async create(@Body() body: CreateNotificationBody) {
 
-  const { recipientId, content, category } = body;
+   const { recipientId, content, category } = body;
 
-  await this.prisma.notification.create({
-    data: {
-      id: randomUUID(),
-      content,
-      category,
-      recipientId,
-    },
+   await this.prisma.notification.create({
+     data: {
+       id: randomUUID(),
+       content,
+       category,
+       recipientId,
+      },
   });
   
 }
