1288748bfcd30e228248dd527d56b13eb1b44fda
Jieff Cavalcanti <jieff.cavalcanti@hotmail.com>
2023-08-06 00:47:59 -0300

    in memory notification and tests

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,26 +1,19 @@
-import { Notification } from "../entities/notification";
+import { InMemoryNotificationsRepository } from "../../../test/repositories/in-memory-notification-repository";
 import { SendNotification } from "./send-notification";
-
-const notifications: Notification[]=[];
-
-const notificationsRepository = {
-    async create(notification: Notification){
-        notifications.push(notification);
-    },
-};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification(notificationsRepository)
 
-    await sendNotification.execute({
+    const notificationRepository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationRepository)
+
+    const { notification } = await sendNotification.execute({
         content: 'This is a notification',
         category: 'social',
         recipientId: 'example recipient-id'
     });
 
-    console.log(notifications);
-
-    expect(notifications).toHaveLength(1);
+    expect(notificationRepository.notifications).toHaveLength(1);
+    expect(notificationRepository.notifications[0]).toEqual(notification);
   })
 });
